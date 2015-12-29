c873cc6fd9d3ab058d155a63e3c44913dfaf713a
m3k3r1 <m3k3r1@gmail.com>
2015-12-29 18:28:04 +0000

    VENICE

--- po/js/functions.js
+++ po/js/functions.js
@@ -6,4 +6,9 @@ $(window).scroll(function(){
   $('.logo').css({
     'transform' : 'translate(0px, '+ wScroll /2 +'%)'
   });
+
+if(wScroll > $('.large-window').offset().top - $(window).height()){
+  $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+  var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+  $('.window-tint').css({'opacity': opacity});}
 });
