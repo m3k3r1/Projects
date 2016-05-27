078fa780599c0265f981b4aad3d60c227c9710bc
m3k3r1 <m3k3r1@gmail.com>
2016-05-27 13:36:37 +0100

    comilao

--- Comilao/comilaofunctions.c
+++ Comilao/comilaofunctions.c
@@ -2,6 +2,7 @@
 
 int menu() {
   int menu_op;
+  system(CLEAR);
 
   printf("   ______                        _   __                  \n" );
   printf(" .' ___  |                      (_) [  |                 \n");
@@ -42,95 +43,163 @@ int startGame(){
   int nrows;
   int ncolumns;
   int winner = FALSE;
-  int player = 0;
-  struct play game;
+  int player = 1;
+  int nplays = 0;
+  int number;
+  char letter;
+
+  system(CLEAR);
+  printf("\n\n\n\n");
 
   getDimensions(&nrows,  &ncolumns);
-  int **board = allocMem( nrows, ncolumns);
+  int **board = allocMem(nrows, ncolumns);
   makeBoard(board, nrows, ncolumns);
-  //showBoard(board, nrows, ncolumns);
+  showBoard(board, nrows, ncolumns);
 
-  while(winner == FALSE){
-    printf("\nCoordenates (A-1)> " );
-    scanf("%c-%d ",&game.letter, &game.number );
+  while (winner == FALSE) {
+    if(player > 2)
+      player = 1;
+    if (player >= 1) {
+    do {
+      getPlay(&letter, &number, player);
+    } while(checkPlay(nrows,ncolumns,letter,number));
+    makePlay(board, nrows, ncolumns, letter, number, player);
+    nplays++;
+    savePlay(board, nrows ,ncolumns, letter, number, nplays,player);
+    showBoard(board, nrows, ncolumns);
+    winner = checkWinner(board, nrows, ncolumns, player);
+    player++;
+    }
   }
-
   free(board);
   return TRUE;
 }
 void getDimensions(int *rows, int *col) {
-  system(CLEAR);
-  printf("\n\n\n\n");
-  printf("\tLines > " );
-  scanf("%d", rows );
-  printf("\tCollums > ");
-  scanf("%d", col);
+  do {
+    printf("\tLinhas (colocar valor entre 4 e 8) > " );
+    scanf("%d", rows );
+  } while(*rows < 4 || *rows  > 8);
+
+  do {
+    printf("\tColunas (colocar valor entre 6 e 10) > ");
+    scanf("%d", col);
+  } while(*col < 6 || *col > 10);
+
+  while(*rows > *col){
+    system(CLEAR);
+    printf("\n\n\n\n");
+    printf("\n\tO numero de colunas deve ser maior que o numero de linhas \n\n");
+    getDimensions(rows, col);
+  }
+
 }
 int **allocMem(int nrows, int ncolumns){
 
-  int **board = malloc(nrows * sizeof(int *));
-  for(int i = 0; i < nrows; i++)
-  	board[i] = malloc(ncolumns * sizeof(int));
+  int **board = malloc( (nrows + 1) * sizeof(int *));
+  for(int i = 0; i < nrows + 1 ; i++)
+  	board[i] = malloc( (ncolumns + 1) * sizeof(int));
 
   if (board == NULL) {
     printf("\t\nERROR::memory allocation\n\n" );
     free(board);
-    return;
+    return 0 ;
   }
 
   return board;
 }
 void makeBoard(int **board, int nrows, int ncolumns){
+  for(int lines = 0; lines < nrows + 1; lines++){
+    for(int collums = 0; collums < ncolumns + 1; collums++){
+      if (lines == (nrows) &&collums == (ncolumns))
+        (*(board + lines))[collums] = 88;
+    else if(lines != 0 && collums != 0)
+      (*(board + lines))[collums] = 42;
+    }
+  }
+}
+void showBoard(int **board, int nrows, int ncolumns){
   system(CLEAR);
   printf("\n\n\n");
-  for(int lines = 0, letter = 65, number = 1; lines < nrows + 1; lines++){
+
+  for(int lines = 0,letter = 65, number = 1; lines < nrows + 1; lines++){
     for(int collums = 0; collums < ncolumns + 1; collums++){
-
       if ( lines == 0  && collums != 0){
-        (*board + lines)[collums] = letter;
-        printf("| %c ", (*board + lines)[collums]);
-        letter++;
-      }
-
-      if (collums == 0 && lines != 0){
-        (*board + lines)[collums] = number;
-        printf("| %d ", (*board + lines)[collums]);
-        number++;
-      }
-
-      if(collums == 0 && lines == 0)
-        printf("    ");
-
-       if (lines == (nrows) &&collums == (ncolumns)){
-        (*board + lines)[collums] = 88;
-        printf("| %c ", (*board + lines)[collums]);
-        }
-
-      else if(lines != 0 && collums != 0){
-        (*board + lines)[collums] = 42;
-        printf("| %c ", (*board + lines)[collums]);
-      }
+        printf("| %c ", letter);
+        letter++;}
+        if (collums == 0 && lines != 0){
+          printf("| %d ", number);
+          number++;}
+        if(collums == 0 && lines == 0)
+          printf("    ");
+        if (lines == (nrows) && collums == (ncolumns))
+          printf("| %c ", (*(board + lines))[collums]);
+        else if(lines != 0 && collums != 0)
+          printf("| %c ", (*(board + lines))[collums]);
     }
     printf("| \n" );
   }
 }
-/**void showBoard(int **board, int nrows, int ncolumns){
-  system(CLEAR);
-  printf("\n\n\n");
-  for(int lines = 0; lines < nrows + 1; lines++){
-    for(int collums = 0; collums < ncolumns + 1; collums++){
-      if ( lines == 0  && collums != 0)
-        printf("| %c ", (*board + lines)[collums]);
-      if (collums == 0 && lines != 0)
-        printf("| %d ", (*board + lines)[collums]);
-      if(collums == 0 && lines == 0)
-        printf("    ");
-      if (lines == (nrows) &&collums == (ncolumns))
-        printf("| %c ", (*board + lines)[collums]);
-      else if(lines != 0 && collums != 0)
-        printf("| %c ", (*board + lines)[collums]);
-    }
-    printf("| \n" );
+void getPlay(char *letter, int* number, int player){
+  printf("\n  ::Jogador %d::  \n",player );
+  printf("\tJogada (Exemplo - A1) > " );
+  scanf(" %c%d", letter, number);
+}
+int checkPlay( int nrows, int ncolumns, char letter, int number){
+  int lim = (letter - 65) + 1;
+
+  if (lim > ncolumns || number > nrows){
+    printf("\tJOGADA INVALIDA\n" );
+    return 1;
   }
- }
-**/
+  return 0;
+
+}
+void makePlay(int **board, int nrows, int ncolumns, char letter, int number, int player){
+  int lim = (letter - 65) + 1;
+  for(int lines = 1; lines < nrows + 1; lines++){
+    for(int collums = 1; collums < ncolumns + 1  ; collums++){
+      if( lines <= number && collums <= lim)
+        (*(board + lines))[collums] = 32;
+    }
+  }
+}
+void savePlay(int **board, int nrows, int ncolumns, char letter, int number, int nplays ,int player){
+  FILE *f;
+   f = fopen("save.txt", "a+");
+
+   if(f==NULL){
+     printf("Erro no acesso ao ficheiro\n");
+     return;}
+
+    fprintf(f, "\n");
+    fprintf(f, "%d: JOGADOR %d > %c%d \n", nplays , player, letter, number);
+    fprintf(f, "\n");
+
+    for(int lines = 0,letter = 65, number = 1; lines < nrows + 1; lines++){
+      for(int collums = 0; collums < ncolumns + 1; collums++){
+        if ( lines == 0  && collums != 0){
+          fprintf(f, "| %c ", letter);
+          letter++;}
+        if (collums == 0 && lines != 0){
+          fprintf(f, "| %d ", number);
+          number++;}
+        if(collums == 0 && lines == 0)
+          fprintf(f,"    ");
+        if (lines == (nrows) && collums == (ncolumns))
+          fprintf(f, "| %c ", (*(board + lines))[collums]);
+        else if(lines != 0 && collums != 0)
+          fprintf(f, "| %c ", (*(board + lines))[collums]);
+      }
+      fprintf(f,"| \n" );
+    }
+    fprintf(f, "\n\n");
+    fclose(f);
+}
+int checkWinner(int **board, int nrows, int ncolumns, int player){
+  if ((*(board + nrows))[ncolumns] == 32){
+    printf(" \n\t\tJOGADOR %d  PERDEU ! \n\n",player );
+    return TRUE;
+  }
+  else
+    return FALSE;
+  }
--- Comilao/include/comilao.h
+++ Comilao/include/comilao.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -10,15 +11,15 @@
 #define CLEAR "clear"
 #endif
 
-struct play {
-  char letter;
-  int number;
-};
-
 int startGame();
 int menu();
 int menu_start();
 void getDimensions(int *rows, int *col);
 int **allocMem(int nrows, int ncolumns);
 void makeBoard(int **board, int nrows, int ncolumns);
-//void showBoard(int **board, int nrows, int ncolumns);
+void showBoard(int **board, int nrows, int ncolumns);
+void getPlay(char *letter, int* number, int player);
+int checkPlay( int nrows, int ncolumns, char letter, int number);
+void makePlay(int **board, int nrows, int ncolumns, char letter, int number, int player);
+void savePlay(int **board, int rows, int collums, char letter, int number, int nplays,int player);
+int checkWinner(int **board, int nrows, int ncolumns, int player);
